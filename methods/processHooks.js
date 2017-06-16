17469d3687ac52cbf433bb59e92d0f4a3b54cca6
Greg Allen <greg@firstandthird.com>
2017-06-16 09:43:33 -0700

    support hook payload

--- methods/processHooks.js
+++ methods/processHooks.js
@@ -3,20 +3,27 @@ const wreck = require('wreck');
 module.exports = function(build, done) {
   const server = this;
   async.each(build, (item, next) => {
+    if (typeof item.hook === 'string') {
+      item.hook = { url: item.hook };
+    }
+    if (!item.hook.payload) {
+      item.hook.payload = {};
+    }
+    item.hook.payload.image = item.image;
     server.log(['hook', 'notice'], {
-      message: `Sending webhook: ${item.hook}`,
-      item
+      message: `Sending webhook: ${item.hook.url}`,
+      payload: item.hook.payload
     });
-    wreck.post(item.hook, {
-      payload: JSON.stringify({
-        image: item.image,
-        item
-      })
+    wreck.post(item.hook.url, {
+      payload: JSON.stringify(item.hook.payload)
     }, (err) => {
       if (err) {
-        server.log(['hook', 'error', item.hook, item.image], err);
+        server.log(['hook', 'error', item.hook.url, item.image], err);
       } else {
-        server.log(['hook', 'success'], item);
+        server.log(['hook', 'success'], {
+          message: `Sent webhook: ${item.hook.url}`,
+          payload: item.hook.payload
+        });
       }
       next();
     });
